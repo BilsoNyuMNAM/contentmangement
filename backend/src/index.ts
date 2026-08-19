1f76e22135100e9c6423ab4e2b17fd0bd7554498
Bilson Yumnam <yumnambilson@gmail.com>
2026-08-19 19:32:58 +0530

    added a route to keep the server active when deployed on rail

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,16 @@ import courseRouter from "./course/route.js";
 import noteRouter from "./notes/route.js";
 import cors from "cors";
 import webHookRouter from "./webhook/route.js";
+const PORT = process.env.PORT || 3000;
 const app = express();
 //  const prisma = new PrismaClient();
-
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 app.use(cors());
 app.use("/api/v1", webHookRouter);
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", noteRouter);
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
